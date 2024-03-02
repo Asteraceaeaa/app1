3656ed4fcf45383b550ed29841ff0bf81ce23efb
Asteraceaeaa <kovinv454@gmail.com>
2024-03-02 17:41:06 +0500

    initilisation of announcements app. Add view and url for it

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from announcements import views as annoucements_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', annoucements_view.annoucementsPage)
 ]
